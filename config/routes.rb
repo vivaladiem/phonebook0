b8054f3c470a052cc5651a0b92eebe7599a36e49
vivaladiem <vivaladiem@naver.com>
2014-02-20 21:12:33 +0900

    전화번호부 만들기 완성2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Phone::Application.routes.draw do
-  get "home/list"
-  get "home/add"
-  get "home/add_process"
+  get "home/list" => "home#list"
+  get "home/add" => "home#add"
+  post "home/add_process" => "home#add_process"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
